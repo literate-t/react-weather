1b4e768604c6b9f6c6f9ab92f556377b88831897
literate-t <gaekoon@gmail.com>
2022-08-10 23:12:14 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'prettier/prettier': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+};
