6ef73a917f68f6b1d620da76dc2e475cff1099e6
literate <gaekoon@gmail.com>
2022-08-20 16:34:06 +0900

    fix: process.env 키 관련 오류

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { format, getBaseDate, getArea, sortByFcstTime } from '../util';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const getPositionFromAddress = async (location) => {
   const result = await axios(
