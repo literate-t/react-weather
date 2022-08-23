ed9f32ffcce88097363aa8877230a9fdb8889008
literate <gaekoon@gmail.com>
2022-08-23 21:12:46 +0900

    fix: 웹팩 설정 덮어쓰기(plugins)

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,17 @@
+const webpack = require('webpack');
+
 module.exports = {
   webpack: (config, env) => {
-    const fallback = config.resolve.fallback || {};
-    Object.assign(fallback, {
-      path: require.resolve('path-browserify'),
-      os: require.resolve('os-browserify'),
-    });
+    const newPlugins = config.plugins.filter(
+      (plugin) => !(plugin instanceof webpack.DefinePlugin)
+    );
+
+    newPlugins.push(
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+      })
+    );
+    config.plugins = newPlugins;
 
     return config;
   },
